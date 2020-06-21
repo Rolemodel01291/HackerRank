cc47afbbc022de68a64d3fb9ad497b9666003e13
Hardanish Singh <hardanishsingh1@gmail.com>
2020-06-20 20:02:21 -0400

    InputAndOutput

--- C++/Introduction/InputAndOutput.cpp
+++ C++/Introduction/InputAndOutput.cpp
@@ -1,18 +1,6 @@
-/*********************************************************************
- *             HACKERRANK INPUT AND OUTPUT CHALLENGE                 *
- *                                                                   *
- * Problem Statement                                                 *
- * Read 3 numbers from stdin and print their sum to stdout.          *
- * Note: If you plan on completing this challenge in C instead of    *
- * C++, you'll need to use format specifiers with printf & scanf.    *
- *                                                                   *
- * Examples                                                          *
- * Input 1: 1 2 7                                                    *
- * Output 1: 10                                                      *
- *                                                                   *
- * Problem URL                                                       *
- * https://www.hackerrank.com/challenges/cpp-input-and-output/problem*
- ********************************************************************/
+/*
+  Problem URL: https://www.hackerrank.com/challenges/cpp-input-and-output/problem
+*/
 
 #include<iostream>
 using namespace std;
